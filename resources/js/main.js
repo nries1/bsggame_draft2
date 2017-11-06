710ab77f641c4cc6e77b74aa5474d9fb77fc4450
U-LMJ53R0Z\NRies <nries1@gmail.com>
2017-11-06 16:39:14 -0500

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,61 @@
+
+function moveViporDown() {
+    let currentPositionObject = $("#vipor").position();
+    let currentDistanceFromTop = currentPositionObject.top;
+    let newDistanceFromTop = Number(currentDistanceFromTop+5);
+    if (currentDistanceFromTop < 466) {
+      $("#vipor").offset({top: newDistanceFromTop});
+    }
+};
+
+function moveViporUp() {
+    let currentPositionObject = $("#vipor").position();
+    let currentDistanceFromTop = currentPositionObject.top;
+    let newDistanceFromTop = Number(currentDistanceFromTop-5);
+    let parentContainerDistanceFromTop = $("#viporZone").position().top
+    if (currentDistanceFromTop > Number(parentContainerDistanceFromTop + 8)) {
+       $("#vipor").offset({top: newDistanceFromTop});
+    };
+};
+
+function fireMissle() {
+    let currentViporPositionObject = $("#vipor").position();
+    let currentViporDistanceFromTop = currentViporPositionObject.top;
+    let currentViporDistanceFromLeft = Number(currentViporPositionObject.left);
+    let newMissileTopPosition = Number(currentViporDistanceFromTop+5);
+    $("#missile").offset({top: newMissileTopPosition});
+    $("#missile").css("visibility", "visible");
+    let firedMissilePosition = currentViporDistanceFromLeft;
+    moveMissile();
+    
+    function moveMissile() {
+    $("#missile").css("visibility", "hidden");
+    firedMissilePosition = firedMissilePosition+1;
+    $("#firedMissile").offset({top: Number(currentViporDistanceFromTop)+3});
+    $("#firedMissile").css("visibility", "visible");
+    $("#firedMissile").offset({left: firedMissilePosition});
+    if(firedMissilePosition < 475) {
+     setTimeout(moveMissile, 1);
+    } else {
+       $("#firedMissile").css("visibility", "hidden");
+       return;
+    };
+};
+};
+
+let missileCounter = 0;
+$(document).on("keydown", function(event) {
+  	if (event.which === 38) {  // if user presses up arrow
+     moveViporUp();
+    } else
+    if (event.which === 40) {  // if user presses down arrow
+     moveViporDown();
+  	} else
+    if (event.which === 32) {  // if user presses space bar
+     missileCounter++;
+     if (missileCounter > 3) {
+         missileCoounter = 0
+     }
+     fireMissle();
+     };
+    });
